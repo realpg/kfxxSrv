9a1f9cd22efdc475af6010f75e596116b84f508f
zhangyiping <874570038@qq.com>
2018-01-26 11:57:02 +0800

    修复获取康复计划接口在没有用户病历的时候报错的情况

--- app/Http/Controllers/API/KFJHController.php
+++ app/Http/Controllers/API/KFJHController.php
@@ -29,27 +29,31 @@ class KFJHController
 		$kfjhs = KFJHManager::getKFJHById($data['id']);
 		if (!$kfjhs) {
 			return ApiResponse::makeResponse(false, '未找到用户病历信息', ApiResponse::INNER_ERROR);
-		} else{
+		} else {
 			$bl = KFJHManager::getBLById($data['id']);
-			$ss_time=strtotime($bl->ss_time);
-			foreach ($kfjhs as $kfjh){
+			if (!$bl) {
+				return ApiResponse::makeResponse(false, '未找到用户病历信息', ApiResponse::INNER_ERROR);
+			}
+			$ss_time = strtotime($bl->ss_time);
+			foreach ($kfjhs as $kfjh) {
 				//手术后
-				if($kfjh->btime_type=='0'){
-					$start_unit=($kfjh->start_unit=='0'?86400:($kfjh->start_unit=='1'?604800:2592000));
-					$kfjh->start_time_stamp=$ss_time+$kfjh->start_time*$start_unit;
-					$kfjh->start_date=date("Y-m-d",$kfjh->start_time_stamp);
-					$end_unit=($kfjh->end_unit='0'?86400:($kfjh->end_unit='1'?604800:2592000));
-					$kfjh->end_time_stamp=$ss_time+$kfjh->end_time*$end_unit;
-					$kfjh->end_date=date("Y-m-d",$kfjh->end_time_stamp);
-				}else if($kfjh->btime_type=='2'){
-					$kfjh->set_date_time_stamp=strtotime($kfjh->set_date);
+				if ($kfjh->btime_type == '0') {
+					$start_unit = ($kfjh->start_unit == '0' ? 86400 : ($kfjh->start_unit == '1' ? 604800 : 2592000));
+					$kfjh->start_time_stamp = $ss_time + $kfjh->start_time * $start_unit;
+					$kfjh->start_date = date("Y-m-d", $kfjh->start_time_stamp);
+					$end_unit = ($kfjh->end_unit = '0' ? 86400 : ($kfjh->end_unit = '1' ? 604800 : 2592000));
+					$kfjh->end_time_stamp = $ss_time + $kfjh->end_time * $end_unit;
+					$kfjh->end_date = date("Y-m-d", $kfjh->end_time_stamp);
+				} else if ($kfjh->btime_type == '2') {
+					$kfjh->set_date_time_stamp = strtotime($kfjh->set_date);
 					//指定时间
 				}
 			}
+			
 		}
-			return ApiResponse::makeResponse(true, $kfjhs, ApiResponse::SUCCESS_CODE);
+		return ApiResponse::makeResponse(true, $kfjhs, ApiResponse::SUCCESS_CODE);
 	}
-
+	
 	//根据ID获取病历
 	public static function getBLByUserId(Request $request)
 	{
@@ -84,7 +88,7 @@ class KFJHController
 			return ApiResponse::makeResponse(false, '未找到用户病历信息', ApiResponse::INNER_ERROR);
 		} else {
 			foreach ($sj as $item)
-			$item = KFJHManager::getKFSJByLevel($item, 2);
+				$item = KFJHManager::getKFSJByLevel($item, 2);
 			//增加按照level获得数据，完善数据细节
 			return ApiResponse::makeResponse(true, $sj, ApiResponse::SUCCESS_CODE);
 		}
